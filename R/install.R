34a3af91dedf4e174848e7550d41deb5bf5cf10c
Jason Conner <jason.conner>
2021-01-06 09:26:53 -0800

    VM minor sync

--- R/install.R
+++ R/install.R
@@ -6,4 +6,8 @@ library(devtools)
 Sys.setenv("R_REMOTES_NO_ERRORS_FROM_WARNINGS"=TRUE)
 
 install_github(repo = "DTUAqua/DATRAS/DATRAS")
+# remotes::install_github("DTUAqua/DATRAS/DATRAS") # for MRAN
+
+
 install_github(repo = "casperwberg/surveyIndex/surveyIndex")
+# remotes::install_github("casperwberg/surveyIndex/surveyIndex") # for MRAN
